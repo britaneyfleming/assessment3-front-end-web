6f35adece932808f653a2e39d8bcef07e7f088a9
Britaney Fleming <britaneyrfleming@gmail.com>
2022-11-19 13:35:13 -0600

    fourth commit-save progress

---
+++ assessment-front-end-web/profile.js
@@ -0,0 +1,14 @@
+let buttonClick = document.getElementById("color") 
+buttonClick.addEventListener('click', function(event){
+	alert("Purple")
+})
+
+let buttonClick2 = document.getElementById("place") 
+buttonClick2.addEventListener('click', function(event){
+	alert("Nassau, Bahamas")
+})
+
+let buttonClick3 = document.getElementById("ritual") 
+buttonClick3.addEventListener('click', function(event){
+	alert("Meditation")
+})
